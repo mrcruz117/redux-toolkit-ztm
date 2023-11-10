ea4aee796640d875c53f40dc34bc071e4ddb8e4c
Michael Cruz <mrcruz1989@gmail.com>
2023-11-10 09:21:52 -0500

    refacctored userReducer for redux toolkit

--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -1,16 +1,21 @@
-import { USER_ACTION_TYPES } from './user.types';
+import { createSlice } from "@reduxjs/toolkit";
+import { USER_ACTION_TYPES } from "./user.types";
 
 const INITIAL_STATE = {
   currentUser: null,
 };
 
-export const userReducer = (state = INITIAL_STATE, action) => {
-  const { type, payload } = action;
+export const userSlice = createSlice({
+  name: "user",
+  initialState: INITIAL_STATE,
+  // This is not a mutation
+  reducers: {
+    setCurrentUser: (state, action) => {
+      state.currentUser = action.payload;
+    },
+  },
+});
 
-  switch (type) {
-    case USER_ACTION_TYPES.SET_CURRENT_USER:
-      return { ...state, currentUser: payload };
-    default:
-      return state;
-  }
-};
+export const { setCurrentUser } = userSlice.actions;
+
+export const userReducer = userSlice.reducer;
